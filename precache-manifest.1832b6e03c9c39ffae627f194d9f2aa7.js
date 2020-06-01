ddfc7f4b58250cff15362c51ce736c0dd3c47329
Carlos González <caal.0522@gmail.com>
2020-06-01 17:26:47 -0500

    Updates

---
+++ precache-manifest.1832b6e03c9c39ffae627f194d9f2aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7df4a0b3330e0c4b28a2d32f6ef183f",
+    "url": "/mymari/index.html"
+  },
+  {
+    "revision": "de2b6f1c2036e23dfd56",
+    "url": "/mymari/static/css/main.4c36d81a.chunk.css"
+  },
+  {
+    "revision": "b0f715d2fae9694aa86e",
+    "url": "/mymari/static/js/2.97ff6d94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mymari/static/js/2.97ff6d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2b6f1c2036e23dfd56",
+    "url": "/mymari/static/js/main.c3993b88.chunk.js"
+  },
+  {
+    "revision": "27944ed1b1d2a42f897a",
+    "url": "/mymari/static/js/runtime-main.7dd3241a.js"
+  }
+]);
