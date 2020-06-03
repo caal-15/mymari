580c1942238ff7e28e3c947454cf4843c5e8249b
Carlos González <caal.0522@gmail.com>
2020-06-03 10:53:56 -0500

    Adds better basename :).

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ThoughtsPage from './ThoughtsPage'
 import NumberedThoughtsPage from './NumberedThoughtsPage'
 
 const App = () => (
-  <BrowserRouter basename="/mymari">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact path="/" component={ThoughtsPage} />
       <Route path="/numbered/:page" component={NumberedThoughtsPage} />
